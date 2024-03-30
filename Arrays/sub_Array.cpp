c687aa08ddbd51660c63e2aa40cde74399da5f28
AditAgrawal05 <aditagrawal565@gmail.com>
2024-03-30 11:06:51 +0530

    Data Structure and Algorithm

--- Arrays/sub_Array.cpp
+++ Arrays/sub_Array.cpp
@@ -5,10 +5,12 @@ int main() {
 	cin>>n;
 	int arr[n];
 	
+	
 	for(int i =0;i<n;i++){
 		cin>>arr[i];
 		}
 	for(int i = 0;i<n;i++){
+		
 	  for(int j =i;j<n;j++){
 	    for(int k = i;k<=j;k++){
 	      cout<<arr[k]<<" ";
